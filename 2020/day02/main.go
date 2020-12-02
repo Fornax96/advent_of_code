f670c54206e38ef08a0f1f27144fd51169ba79fd
Wim Brand <wb@voclarion.nl>
2020-12-02 12:02:11 +0100

    Add 2020 day2
    day1 will be added later

---
+++ 2020/day02/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Answer 1:")
+	part1()
+	fmt.Println("Answer 2:")
+	part2()
+}
+
+func part1() {
+	file, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	var matches = 0
+
+	for scanner.Scan() {
+		split := strings.SplitN(scanner.Text(), ": ", 2)
+		conditions, password := split[0], split[1]
+
+		split = strings.SplitN(conditions, " ", 2)
+		limits, letter := split[0], split[1]
+
+		split = strings.SplitN(limits, "-", 2)
+		min, err := strconv.Atoi(split[0])
+		if err != nil {
+			panic(err)
+		}
+		max, err := strconv.Atoi(split[1])
+		if err != nil {
+			panic(err)
+		}
+
+		count := strings.Count(password, letter)
+		if count >= min && count <= max {
+			matches++
+		}
+	}
+
+	fmt.Println(matches)
+}
+
+func part2() {
+	file, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	var matches = 0
+
+	for scanner.Scan() {
+		split := strings.SplitN(scanner.Text(), ": ", 2)
+		conditions, password := split[0], split[1]
+
+		split = strings.SplitN(conditions, " ", 2)
+		limits, letter := split[0], split[1]
+
+		split = strings.SplitN(limits, "-", 2)
+		pos1, err := strconv.Atoi(split[0])
+		if err != nil {
+			panic(err)
+		}
+		pos2, err := strconv.Atoi(split[1])
+		if err != nil {
+			panic(err)
+		}
+
+		var letterMatch = 0
+		if password[pos1-1] == letter[0] {
+			letterMatch++
+		}
+		if password[pos2-1] == letter[0] {
+			letterMatch++
+		}
+
+		if letterMatch == 1 {
+			matches++
+		}
+	}
+
+	fmt.Println(matches)
+}
