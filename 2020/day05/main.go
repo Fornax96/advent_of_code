3b385cc3296e4b65e2aac485621a55536c09b417
Wim Brand <fornax@fornaxian.com>
2020-12-05 14:35:42 +0100

    stop being a dummy

--- 2020/day05/main.go
+++ 2020/day05/main.go
@@ -16,62 +16,51 @@ func main() {
 
 func part1(lines []string) (idTotal int) {
 	for _, line := range lines {
-		var row, col = 0, 0
+		var id = 0
 		for _, c := range line {
 			switch c {
-			case 'F': // Front
-				row = row << 1
-			case 'B': // Back
-				row = (row << 1) + 1
-			case 'L': // Left
-				col = col << 1
-			case 'R': // Right
-				col = (col << 1) + 1
+			case 'F', 'L': // Shift 0
+				id = id << 1
+			case 'B', 'R': // Shift 1
+				id = (id << 1) + 1
 			}
 		}
 
-		if (row*8)+col > idTotal {
-			idTotal = (row * 8) + col
+		if id > idTotal {
+			idTotal = id
 		}
 	}
-
 	return idTotal
 }
 
-func part2(lines []string) (myID int) {
+func part2(lines []string) int {
 	var ids = make(map[int]bool)
 
 	for _, line := range lines {
-		var row, col = 0, 0
+		var id = 0
 		for _, c := range line {
 			switch c {
-			case 'F': // Front
-				row = row << 1
-			case 'B': // Back
-				row = (row << 1) + 1
-			case 'L': // Left
-				col = col << 1
-			case 'R': // Right
-				col = (col << 1) + 1
+			case 'F', 'L': // Shift 0
+				id = id << 1
+			case 'B', 'R': // Shift 1
+				id = (id << 1) + 1
 			}
 		}
-		ids[(row*8)+col] = true
+		ids[id] = true
 	}
 
 	// Find the missing id
-	var h1, h2, h3 = false, false, false
-	var lastID = 0
-
-	for row := 1; row < 127; row++ {
+	var h1, h2, h3, lastID = false, false, false, 0
+	for row := 0; row < 128; row++ {
 		for col := 0; col < 8; col++ {
 			h3 = h2
 			h2 = h1
-			h1 = ids[(row*8)+col]
+			h1 = ids[(row<<3)+col]
 
 			if h3 && !h2 && h1 {
 				return lastID
 			}
-			lastID = (row * 8) + col
+			lastID = (row << 3) + col
 		}
 	}
 	return 0
